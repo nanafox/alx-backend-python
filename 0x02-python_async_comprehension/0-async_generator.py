ce62585020659800855ab86359df131cb0665f86
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2024-07-09 12:57:22 +0000

    refactor: Refactor async_generator to use Generator type instead of AsyncGenerator

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -11,10 +11,10 @@ yield a random number between 0 and 10. Use the `random` module.
 
 import asyncio
 import random
-from typing import AsyncGenerator
+from typing import Generator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> Generator[float, None, None]:  # type: ignore
     """
     This coroutine yields 10 random float numbers between 0 and 10.
     """
